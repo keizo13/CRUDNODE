2e0ee83da412ab414c5f71f9f9cccbfbcb8aacdd
keizo <13keizo@gmail.com>
2022-08-19 15:05:24 -0400

    corrigido

--- app/controllers/usercontroller.js
+++ app/controllers/usercontroller.js
@@ -24,10 +24,7 @@ const Crypto = require('../utils/crypto.js');
     async findOneUser(req, res) {
         try {
             const { id } = req.params;
-            const user = await User.findByPk(id);
-            if (!user) {
-                throw new Error("Usuário não existe");
-            }            
+            const user = await  this.validateUser(id);
             res.send(user);
         } catch(e) {
             res.status(404).send({ error: e.message });
@@ -38,14 +35,9 @@ const Crypto = require('../utils/crypto.js');
         try { 
             const { id } = req.params;
             const { name, email, password, image } = req.body;
-            const user = await User.findByPk(id);
-            if (!user) {
-                throw new Error("Usuário não existe");
-            }
-            const isValidPassword = await Crypto.comparePasswords(password, user.password);
-            if (!isValidPassword) {
-                throw new Error("Senha incorreta");
-            }
+            const user = await  this.validateUser(id);
+            const isValidPassword = await Crypto.comparePasswords(password, user.password);        
+            await this.validatePassword(isValidPassword);
             const updateUser = await User.update({
                 name: name || user.name,
                 email: email || user.email,                
@@ -56,9 +48,9 @@ const Crypto = require('../utils/crypto.js');
                 }
             }
             );
-            if(!updateUser[0]) {
-                throw new Error("Não foi possível atualizar o usuário!");
-            } 
+            if(!updateUser[0]){
+            await this.validateUpdate();
+            }
             res.sendStatus(204);     
             
 
@@ -78,15 +70,10 @@ const Crypto = require('../utils/crypto.js');
             const { id } = req.params;
             const { password, newPassword } = req.body;
             const myEncryptPassword = await Crypto.encryptPassword(newPassword);
-            const user = await User.findByPk(id);
-            if (!user) {
-                throw new Error("Usuário não encontrado!"); 
-            }
-            const isValidPassword = await Crypto.comparePasswords(password, user.password);
-            if (!isValidPassword) {
-                throw new Error("Senha incorreta!"); 
-            }
-            const response = await User.update({
+            const user = await  this.validateUser(id);          
+            const isValidPassword = await Crypto.comparePasswords(password, user.password);        
+            await this.validatePassword(isValidPassword);
+            const updatePassword = await User.update({
                 password: myEncryptPassword
             }, {
                 where: {
@@ -94,13 +81,33 @@ const Crypto = require('../utils/crypto.js');
                 }
             }
             );
-            if(response){
-                res.status(200).json({message: "Senha atualizada!"});
-            }
+            if(!updatePassword[0])
+                {
+                await this.validateUpdate();
+                }
+            res.sendStatus(204);  
     }   catch(e)  {
         res.status(400).send({error: e.message});  
     }
   }
+
+    async validatePassword(isValidPassword){
+        if (!isValidPassword) {
+            throw new Error("Senha incorreta!"); 
+        }
+    }
+
+    async validateUpdate(){
+            throw new Error("Não foi possível atualizar o usuário!");
+    }
+
+    async validateUser(id) {
+        const user = await User.findByPk(id);
+        if (!user) {
+            throw new Error("Usuário não existe");
+        } 
+        return user;
+    }
 }
 
   module.exports = new UserController();
